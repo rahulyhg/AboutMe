6a1e4527e7a5f28310032b3afb877cdf251e936c
Raman Skaskevich <lusersks@gmail.com>
2017-03-06 06:59:58 +0100

    Order Main activity. Add About activity.

--- app/src/main/java/com/gmail/lusersks/aboutme/MainActivity.java
+++ app/src/main/java/com/gmail/lusersks/aboutme/MainActivity.java
@@ -1,7 +1,9 @@
 package com.gmail.lusersks.aboutme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View view = findViewById(R.id.tab_about);
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent abAct = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(abAct);
+            }
+        });
     }
 }
